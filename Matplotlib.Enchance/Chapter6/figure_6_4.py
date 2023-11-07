4b059c4639320d52965230d9a86fe0f72e039a44
hexu <hexu_1985@sina.com>
2023-11-07 14:45:47 +0800

    update Matplotlib.Enchance/Chapter6

--- Matplotlib.Enchance/Chapter6/figure_6_4.py
+++ Matplotlib.Enchance/Chapter6/figure_6_4.py
@@ -11,7 +11,7 @@ ax[0].plot(x,y,lw=3,color="dodgerblue")
 
 ax[0].set_ylim(-2,2)
 
-ax[0].set_axis_bgcolor("lemonchiffon")
+ax[0].set_facecolor("lemonchiffon")
 
 # subplot(3,1,2)
 ax[1].plot(x,y,lw=3,color="dodgerblue")
@@ -24,7 +24,7 @@ ax[1].yaxis.set_ticks_position("left")
 
 ax[1].set_ylim(-3,3)
 
-ax[1].set_axis_bgcolor("lemonchiffon")
+ax[1].set_facecolor("lemonchiffon")
 
 # subplot(3,1,3)
 ax[2].plot(x,y,lw=3,color="dodgerblue")
@@ -40,7 +40,7 @@ ax[2].spines["bottom"].set_bounds(0,2*np.pi)
 
 ax[2].set_ylim(-2,2)
 
-ax[2].set_axis_bgcolor("lemonchiffon")
+ax[2].set_facecolor("lemonchiffon")
 
 fig.subplots_adjust(hspace=0.3)
 
