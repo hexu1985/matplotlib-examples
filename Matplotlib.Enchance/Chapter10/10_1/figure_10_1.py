445e58798f4d81a4a24ef979881ca7086d85fcca
hexu <hexu_1985@sina.com>
2024-03-19 16:39:16 +0800

    update Matplotlib.Enchance/Chapter10/10_1/figure_10_1.py

--- Matplotlib.Enchance/Chapter10/10_1/figure_10_1.py
+++ Matplotlib.Enchance/Chapter10/10_1/figure_10_1.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 from matplotlib.cbook import get_sample_data
 from matplotlib.patches import Circle
 
+import pathlib
 
-with get_sample_data("d://sunflower.png",asfileobj=True) as imageFile:
+current_dir = pathlib.Path(__file__).parent
+print(current_dir)
+
+with get_sample_data(current_dir/"sunflower.png",asfileobj=True) as imageFile:
     imageArray = plt.imread(imageFile)
 
 fig,ax = plt.subplots(1,1)
